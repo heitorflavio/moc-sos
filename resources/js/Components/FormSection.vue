d5f077561067df8e0b82c9222845bef3583a969e
heitorflavio <heitorflavio.r@outlook.com>
2024-11-12 16:59:11 -0300

    :recycle: refac: Adiciona suporte a modo escuro nos componentes de ActionSection, FormSection, InputLabel, PrimaryButton, SectionTitle e TextInput

--- resources/js/Components/FormSection.vue
+++ resources/js/Components/FormSection.vue
@@ -21,7 +21,8 @@ const hasActions = computed(() => !! useSlots().actions);
         <div class="mt-5 md:mt-0 md:col-span-2">
             <form @submit.prevent="$emit('submitted')">
                 <div
-                    class="px-4 py-5 bg-white sm:p-6 shadow"
+                    class="px-4 py-5 bg-white sm:p-6 shadow dark:bg-gray-800 sm
+                    :rounded-tl-md sm:rounded-tr-md sm:dark:text-gray-200"
                     :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'"
                 >
                     <div class="grid grid-cols-6 gap-6">
@@ -29,7 +30,8 @@ const hasActions = computed(() => !! useSlots().actions);
                     </div>
                 </div>
 
-                <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md
+                    dark:bg-gray-800 sm:dark:text-gray-200">
                     <slot name="actions" />
                 </div>
             </form>
--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -21,7 +21,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm dark:bg-gray-700 dark:border-gray-600 dark:focus:border-indigo-500 dark:focus:ring-indigo-500 dark:text-gray-300 focus:ring focus:ring-opacity-50"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     >
